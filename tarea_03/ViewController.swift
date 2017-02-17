fc6192be5c5584f8606a2fb01187297946facf77
jose luis yana <joseyananicolas@gmail.com>
2017-02-16 21:10:39 -0500

    Ejercicio en clase con temas de UIAlertController y Stack Views

--- tarea_03/ViewController.swift
+++ tarea_03/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var alert: UIAlertController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnAceptarTouch(_ sender: UIButton) {
+        
+        alert = UIAlertController(title: "Mensaje", message: "Presiono Aceptar", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let actionOK = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in print("Presiono OK")})
+        
+        alert?.addAction(actionOK)
+        
+        self.present(alert!, animated: true)
+    }
+    
+    @IBAction func btnEliminarTouch(_ sender: UIButton) {
+        
+        alert = UIAlertController(title: "Mensaje", message: "Presiono Eliminar", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let actionDestructive = UIAlertAction(title: "Eliminar", style: UIAlertActionStyle.destructive, handler: { (action) in print("Presiono Eliminar")})
+        
+        alert?.addAction(actionDestructive)
+        
+        self.present(alert!, animated: true)
+    }
+    
+    @IBAction func btnCancelarTouch(_ sender: UIButton) {
+        
+        alert = UIAlertController(title: "Mensaje", message: "Presiono Cancelar", preferredStyle: UIAlertControllerStyle.alert)
+        
+        let actionCancel = UIAlertAction(title: "Cancelar", style: UIAlertActionStyle.cancel, handler: { (action) in print("Presiono Cancelar")})
+        
+        alert?.addAction(actionCancel)
+        
+        self.present(alert!, animated: true)
+    }
 
 }
 
